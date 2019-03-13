3b51a1301fbfb7104f25803cb1a7434636128bc1
Paulo Costa <paulombcosta@gmail.com>
2019-03-13 18:30:56 -0300

    WIP

--- linux/install_linuxbrew.sh
+++
@@ -1,5 +0,0 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.zshrc && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zshrc
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
