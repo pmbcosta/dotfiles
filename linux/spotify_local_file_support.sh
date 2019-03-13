09f0e3be1137baa758442ecb4992ca3364bf35c9
Paulo Costa <paulombcosta@gmail.com>
2019-03-13 18:23:05 -0300

    Organizing stuff

--- linux/spotify_local_file_support.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install ubuntu-restricted-extras
-sudo bash -c "wget https://github.com/ramedeiros/spotify_libraries/raw/master/libavcodec.so.54.71.100 -O /usr/lib/x86_64-linux-gnu/libavcodec.so.54 && wget https://github.com/ramedeiros/spotify_libraries/raw/master/libavformat.so.54.36.100 -O /usr/lib/x86_64-linux-gnu/libavformat.so.54 && wget https://github.com/ramedeiros/spotify_libraries/raw/master/libavutil.so.52.6.100 -O /usr/lib/x86_64-linux-gnu/libavutil.so.52 && ldconfig"
